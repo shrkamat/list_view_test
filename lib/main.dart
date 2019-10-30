912d2dcc0ad8e7da76eb09c1ec0a0392c7fd95db
Shrinivas Kamath -X (shrkamat - TRITON UK BIDCO LIMITED c/o Alter Domus (UK) Limited -OBO at Cisco) <shrkamat@cisco.com>
2019-10-31 00:27:57 +0530

    show default image, if fetch fails

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -8,32 +11,73 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      home: ImageDisplay(),
+      home: new Scaffold(
+          appBar: new AppBar(
+            title: Text("IVP"),
+          ),
+          body: Container(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: <Widget>[
+                ImageDisplay(
+                  offset: 120,
+                ),
+                ImageDisplay(
+                  offset: 177,
+                ),
+                Container(
+                  height: 120,
+                  color: Colors.amber,
+                  child: CachedNetworkImage(
+                    imageUrl:
+                        "http://ipsumimage.appspotiii.com/140x100,ff7700?l=999",
+                    placeholder: (context, url) => CircularProgressIndicator(),
+                    // errorWidget: (context, url, error) => Image.network("http://ipsumimage.appspot.com/140x100,ff7700?l=ERR"),
+                    errorWidget: (context, url, error) => Image.asset(
+                      "images/poster.jpg",
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )),
     );
   }
 }
 
 class ImageDisplay extends StatelessWidget {
-  const ImageDisplay({Key key}) : super(key: key);
+  final int offset;
+  const ImageDisplay({Key key, this.offset}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: Text("Image(s)"),
-      ),
-      body: Container(
-        padding: EdgeInsets.only(top:2),
-        height: 200,
-        child: new ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemCount: 10,
-          itemBuilder: (BuildContext cxt, int index) {
-            return Image.network(
-                "http://ipsumimage.appspot.com/140x100,ff7700?l=" +
-                    index.toString());
-          },
-        ),
+    return Container(
+      height: 120,
+      child: new ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 11,
+        itemBuilder: (BuildContext cxt, int index) {
+          return Image.network(
+            "http://ipsumimage.appspot.com/140x100,ff7700?l=" +
+                (offset + index).toString(),
+            loadingBuilder: (BuildContext context, Widget child,
+                ImageChunkEvent loadingProgress) {
+              if (loadingProgress == null) {
+                return child;
+              } else {
+                return Center(
+                  child: CircularProgressIndicator(
+                    value: loadingProgress.expectedTotalBytes != null
+                        ? loadingProgress.cumulativeBytesLoaded /
+                            loadingProgress.expectedTotalBytes
+                        : null,
+                  ),
+                );
+              }
+            },
+          );
+        },
       ),
     );
   }
